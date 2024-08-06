a8664bbda779e0dfae41443a67fff712920cdc4d
Emiliano Perez <51446283+emiperez997@users.noreply.github.com>
2024-08-06 09:04:48 -0300

    [FIX] Courses

--- src/course/interfaces/CreateCourseData.ts
+++ src/course/interfaces/CreateCourseData.ts
@@ -3,11 +3,9 @@ import { CourseStatus } from "@prisma/client";
 export interface CreateCourseData {
   title?: string;
   description?: string;
-  classNumber?: number;
   beginDate?: Date;
   endDate?: Date;
   status?: CourseStatus;
-  categories?: string[];
   teacher?: {
     connect: {
       id: number;
